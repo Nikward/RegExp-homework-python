be0a99a43fe0369817c9beba286f80820ed18059
Nikolay Zolotuhin <nik.ward@yandex.ru>
2023-01-03 21:33:31 +0300

    corrected the design

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import re
 csv_file = 'phonebook_raw.csv'
 PHONE_PATTERN = r"(\+7|8)*[\s\(]*(\d{3})[\)\s-]*(\d{3})[-]*(\d{2})[-]*(\d{2})[\s\(]*(доб\.)?[\s]?(\d{4})*\)?"
 PHONE_SUB = r"+7(\2)-\3-\4-\5 \6\7"
+
 def open_csv(name_file, delimiter) -> list:
     with open(name_file, 'r', encoding='utf-8') as file:
         return list(csv.reader(file, delimiter=delimiter))
+
 def get_full_name(line: list) -> list:
     return " ".join(line[:3]).split(' ')
+
 def get_full_list(contact: list,PHONE_PATTERN,PHONE_SUB) -> list:
     full_list = []
     for line in contact:
@@ -28,6 +31,7 @@ def removing_duplicates(full_list)-> dict:
         else:
             res_dict[name[0]] = name[1:]
     return res_dict
+
 def dict_to_list(data: dict)-> list:
     list_for_csv = []
     for key, values in data.items():
@@ -37,12 +41,12 @@ def dict_to_list(data: dict)-> list:
             temporarily_list.append(val)
         list_for_csv.append(temporarily_list)
     return list_for_csv
+
 def save_csv(data, delimiter):
         with open("contact_book.csv", 'w', encoding='utf-8', newline='') as file:
             write = csv.writer(file, delimiter=delimiter)
             for row in data:
                 write.writerow(row)
-
 
 if __name__ == '__main__':
     open_file = open_csv(csv_file, ',')
